1146909f12de9050e5468497145e154bcf0d82f5
nadsondejesusalmeida <developer.brownnose603@slmails.com>
2025-09-19 23:35:39 -0300

    Melhoria no desempenho

---
+++ assets/scripts/theme.js
@@ -0,0 +1,25 @@
+const themeColorButton = document.getElementById('theme-color-button');
+const themeSystem = localStorage.getItem('themeSystem') || 'light';
+
+function setThemeSystem(theme) {
+	const lightThemeIcon = `<ion-icon name="sunny" class="theme-color-icon"></ion-icon>`;
+	const darkThemeIcon = `<ion-icon name="moon" class="theme-color-icon"></ion-icon>`;
+	
+	document.documentElement.setAttribute('data-theme', theme);
+	
+	if (theme === 'light') {
+		themeColorButton.innerHTML = lightThemeIcon;
+	} else {
+		themeColorButton.innerHTML = darkThemeIcon;
+	}
+}
+
+themeColorButton.addEventListener('click', (e) => {
+	const oldTheme = localStorage.getItem('themeSystem') || 'light';
+	const newTheme = oldTheme === 'light' ? 'dark' : 'light';
+	
+	localStorage.setItem('themeSystem', newTheme);
+	setThemeSystem(newTheme);
+});
+
+setThemeSystem(themeSystem);
