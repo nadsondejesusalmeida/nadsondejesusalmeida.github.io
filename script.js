8aede429e998b6a52c81dd726d1084cf6f74c2aa
nadsondejesusalmeida <developer.brownnose603@slmails.com>
2025-12-29 16:59:28 -0300

    Pequenas mudanças

---
+++ script.js
@@ -0,0 +1,17 @@
+const navLinks = document.querySelectorAll('#navigation-bar ul li a.link');
+
+navLinks.forEach(link => {
+	link.addEventListener('click', function(e) {
+		e.preventDefault();
+		const target = document.querySelector(this.getAttribute('href'));
+		
+		if (target) {
+			const headerHeight = document.querySelector('header').offsetHeight;
+			const targetPosition = target.offsetTop - headerHeight - 20;
+			window.scrollTo({
+				top: targetPosition,
+				behavior: 'smooth'
+			});
+		}
+	});
+});
