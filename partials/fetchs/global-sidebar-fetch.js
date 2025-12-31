b0668e688ece1ae8b0fb8fa6948e0315c5e9932a
nadsondejesusalmeida <developer.brownnose603@slmails.com>
2025-12-31 15:03:03 -0300

    Versão temporária

--- partials/fetchs/global-sidebar-fetch.js
+++ partials/fetchs/global-sidebar-fetch.js
@@ -1,60 +1,62 @@
-const body = document.body;
-
-const websiteURL = 'https://nadsondejesusalmeida.github.io/';
-const globalSidebarURL = `${websiteURL}partials/global-sidebar/global-sidebar.html`;
-const openGlobalSidebarButtonURL = `${websiteURL}partials/buttons/open-global-sidebar-button.html`;
-const globalSidebarOverlayURL = `${websiteURL}partials/overlays/global-sidebar-overlay.html`;
-
-fetch(globalSidebarURL)
-	.then(response => response.text())
-	.then(data => {
-		const tempElement = document.createElement('div');
-		tempElement.innerHTML = data;
-		
-		while (tempElement.firstChild) {
-			if (body.children.length > 0) {
-				body.insertBefore(tempElement.firstChild, body.firstChild);
+document.addEventListener('DOMContentLoaded', () => {
+	const body = document.body;
+	
+	const websiteURL = 'https://nadsondejesusalmeida.github.io/';
+	const globalSidebarURL = `${websiteURL}partials/global-sidebar/global-sidebar.html`;
+	const openGlobalSidebarButtonURL = `${websiteURL}partials/buttons/open-global-sidebar-button.html`;
+	const globalSidebarOverlayURL = `${websiteURL}partials/overlays/global-sidebar-overlay.html`;
+	
+	fetch(globalSidebarURL)
+		.then(response => response.text())
+		.then(data => {
+			const tempElement = document.createElement('div');
+			tempElement.innerHTML = data;
+			
+			while (tempElement.firstChild) {
+				if (body.children.length > 0) {
+					body.insertBefore(tempElement.firstChild, body.firstChild);
+				} else {
+					body.appendChild(tempElement.firstChild);
+				}
+			}
+		})
+	
+	fetch(openGlobalSidebarButtonURL)
+		.then(response => response.text())
+		.then(data => {
+			let header;
+			if (!document.querySelector('header')) {
+				header = document.createElement('header');
+				
+				if (body.firstChild) {
+					body.insertBefore(header, document.body.firstChild);
+				} else {
+					body.appendChild(header);
+				}
 			} else {
+				header = document.querySelector('header');
+			}
+			
+			const tempElement = document.createElement('div');
+			tempElement.innerHTML = data;
+			
+			while (tempElement.firstChild) {
+				if (header.children.length > 0) {
+					header.insertBefore(tempElement.firstChild, header.firstChild);
+				} else {
+					header.appendChild(tempElement.firstChild)
+				}
+			}
+		});
+	
+	fetch(globalSidebarOverlayURL)
+		.then(response => response.text())
+		.then(data => {
+			const tempElement = document.createElement('div');
+			tempElement.innerHTML = data;
+			
+			while (tempElement.firstChild) {
 				body.appendChild(tempElement.firstChild);
 			}
-		}
-	})
-
-fetch(openGlobalSidebarButtonURL)
-	.then(response => response.text())
-	.then(data => {
-		let header;
-		if (!document.querySelector('header')) {
-			header = document.createElement('header');
-			
-			if (body.firstChild) {
-				body.insertBefore(header, document.body.firstChild);
-			} else {
-				body.appendChild(header);
-			}
-		} else {
-			header = document.querySelector('header');
-		}
-		
-		const tempElement = document.createElement('div');
-		tempElement.innerHTML = data;
-		
-		while (tempElement.firstChild) {
-			if (header.children.length > 0) {
-				header.insertBefore(tempElement.firstChild, header.firstChild);
-			} else {
-				header.appendChild(tempElement.firstChild)
-			}
-		}
-	});
-
-fetch(globalSidebarOverlayURL)
-	.then(response => response.text())
-	.then(data => {
-		const tempElement = document.createElement('div');
-		tempElement.innerHTML = data;
-		
-		while (tempElement.firstChild) {
-			body.appendChild(tempElement.firstChild);
-		}
-	});
+		});
+});
